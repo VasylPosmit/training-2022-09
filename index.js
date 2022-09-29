856519c335e2c6024c2ca1ca10223f794649ad85
TGacek89 <gaco.g4@gmail.com>
2022-09-29 16:09:23 +0200

    Basic calculator with some lazynes added

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
 // index.js
+// program for a simple calculator
+let result;
 
-alert("HELLO ITS MY FIRST COMMIT");
+// user input
+const operator = prompt('Enter operator ( either +, -, * or / ) and remember I am lazy ');
+
+// user numbers
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+switch(operator) {
+    case '+':
+         result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        if(result > 1000) {
+            console.log("its to much for me because I'm lazy!")
+        }
+        break;
+
+    case '-':
+         result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        if(result > 1000) {
+            console.log("its to much for me because I'm lazy!")
+        }
+        break;
+
+    case '*':
+         result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        if(result > 1000) {
+            console.log("its to much for me because I'm lazy!")
+        }
+        break;
+
+    case '/':
+         result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        if(result > 1000) {
+            console.log("its to much for me because I'm lazy!")
+        }
+        break;
+      
+    default:
+        console.log('Invalid operator');
+        break;
+}
