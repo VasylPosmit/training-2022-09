09f4055a7c348258b8d1e9690fcc215821a4ccd6
celpjmnz <celpjmnz@gmail.com>
2022-09-29 16:54:37 +0200

    lazyrulesfix

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ alert("HELLO ITS MY FIRST COMMIT");
 
 function filter(res) {
   if (isNaN(res)) {
-    return console.log("Just numbers");
+    console.log("Just numbers");
   } else {
     if (res > 1000) {
-      return console.log("That's too much for me, sorry");
+      console.log("That's too much for me, sorry");
     } else {
       if (res % 1 === 0) {
         let resF = res.toFixed();
-        return console.log(resF);
+        console.log(resF);
       }
     }
   }
