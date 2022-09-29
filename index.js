91feef97167a45045afc22eb1847b5f04c42137f
celpjmnz <celpjmnz@gmail.com>
2022-09-29 16:37:34 +0200

    lazyrules

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // index.js
 
-
 alert("HELLO ITS MY FIRST COMMIT");
 
-//pull request
+function filter(res) {
+  if (isNaN(res)) {
+    return console.log("Just numbers");
+  } else {
+    if (res > 1000) {
+      return console.log("That's too much for me, sorry");
+    } else {
+      if (res % 1 === 0) {
+        let resF = res.toFixed();
+        return console.log(resF);
+      }
+    }
+  }
+}
 
+//pull request
