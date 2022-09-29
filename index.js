117f50f6e84da1f8ee0d14bec704e3118016c568
VladimirGOU <insta000111@ukr.net>
2022-09-29 16:34:23 +0200

    fibonacci sequence

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-// index.js
-// Hi this is Volodymyr __
+const number = parseInt(prompt('Enter the number of terms: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
