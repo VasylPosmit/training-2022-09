dd910d638d1135be2245dc8dc454f2523769be4a
TGacek89 <gaco.g4@gmail.com>
2022-09-30 09:29:04 +0200

    deleted unnecesary code

--- index.js
+++ index.js
@@ -14,37 +14,47 @@ switch(operator) {
     case '+':
          result = number1 + number2;
         console.log(`${number1} + ${number2} = ${result}`);
-        if(result > 1000) {
-            console.log("its to much for me because I'm lazy!")
-        }
         break;
 
     case '-':
          result = number1 - number2;
         console.log(`${number1} - ${number2} = ${result}`);
-        if(result > 1000) {
-            console.log("its to much for me because I'm lazy!")
-        }
         break;
 
     case '*':
          result = number1 * number2;
         console.log(`${number1} * ${number2} = ${result}`);
-        if(result > 1000) {
-            console.log("its to much for me because I'm lazy!")
-        }
         break;
 
     case '/':
          result = number1 / number2;
         console.log(`${number1} / ${number2} = ${result}`);
-        if(result > 1000) {
-            console.log("its to much for me because I'm lazy!")
-        }
         break;
       
     default:
         console.log('Invalid operator');
         break;
+}
+
+//calculator from console only with functions
+
+function addNums(...nums){
+    let res = nums.reduce((num1, num2) => num1 + num2)
+    return res
+}
+
+function subNums(...nums){
+    let res = nums.reduce((num1, num2) => num1 - num2)
+    return res
+}
+
+function divNums(...nums){
+    let res = nums.reduce((num1, num2) => num1 / num2)
+    return res
+}
+
+function multiNums(...nums){
+    let res = nums.reduce((num1, num2) => num1 * num2)
+    return res
 }
 
