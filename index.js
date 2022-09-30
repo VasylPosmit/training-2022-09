2bdb74d206f26c53d4e90c198a4b93cf0e110ba8
celpjmnz <celpjmnz@gmail.com>
2022-09-30 10:08:03 +0200

    limit0

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ function filter(res) {
   if (isNaN(res)) {
     console.log("Just numbers");
   } else {
-    if (res > 1000) {
-      console.log("That's too much for me, sorry");
-    } else {
-      if (res % 1 === 0) {
-        let resF = res.toFixed();
-        console.log("I don't know... Must be something around "+resF);
+    if (res >= 0) {
+      if (res > 1000) {
+        console.log("That's too much for me, sorry");
+      } else {
+        if (res % 1 === 0) {
+          let resF = res.toFixed();
+          console.log("I don't know... Must be something around " + resF);
+        }
       }
+    } else {
+      console.log("That's too slow, sorry");
     }
   }
 }
